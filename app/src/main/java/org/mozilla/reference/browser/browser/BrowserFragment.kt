e91160fdeb0ea7e096f4f9e08ddeceb3da1656e6
zorbeytorunoglu <zorbeytorunoglu@gmail.com>
2024-12-07 18:37:57 +0300

    perf: Optimize BrowserFragment init
    - move component init to coroutine for non-blocking startup
    - eager views to lazy properties
    - focused methods for organization and easier management
    
    result data will be in the document

--- app/src/main/java/org/mozilla/reference/browser/browser/BrowserFragment.kt
+++ app/src/main/java/org/mozilla/reference/browser/browser/BrowserFragment.kt
@@ -6,9 +6,14 @@ package org.mozilla.reference.browser.browser
 
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.lifecycleScope
 import androidx.preference.PreferenceManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import mozilla.components.browser.state.selector.selectedTab
 import mozilla.components.browser.thumbnails.BrowserThumbnails
 import mozilla.components.browser.toolbar.BrowserToolbar
@@ -39,16 +44,122 @@ class BrowserFragment : BaseBrowserFragment(), UserInteractionHandler {
     private val webExtToolbarFeature = ViewBoundFeatureWrapper<WebExtensionToolbarFeature>()
     private val windowFeature = ViewBoundFeatureWrapper<WindowFeature>()
 
-    private val awesomeBar: AwesomeBarWrapper
-        get() = requireView().findViewById(R.id.awesomeBar)
-    private val toolbar: BrowserToolbar
-        get() = requireView().findViewById(R.id.toolbar)
-    private val engineView: EngineView
-        get() = requireView().findViewById<View>(R.id.engineView) as EngineView
-    private val readerViewBar: ReaderViewControlsBar
-        get() = requireView().findViewById(R.id.readerViewBar)
-    private val readerViewAppearanceButton: FloatingActionButton
-        get() = requireView().findViewById(R.id.readerViewAppearanceButton)
+    private val awesomeBar by lazy { requireView().findViewById<AwesomeBarWrapper>(R.id.awesomeBar) }
+    private val toolbar by lazy { requireView().findViewById<BrowserToolbar>(R.id.toolbar) }
+    private val engineView by lazy { requireView().findViewById<View>(R.id.engineView) as EngineView }
+    private val readerViewBar by lazy { requireView().findViewById<ReaderViewControlsBar>(R.id.readerViewBar) }
+    private val readerViewAppearanceButton by lazy {
+        requireView().findViewById<FloatingActionButton>(R.id.readerViewAppearanceButton)
+    }
+
+    private fun initializeAwesomeBar() {
+        AwesomeBarFeature(awesomeBar, toolbar, engineView)
+            .addSearchProvider(
+                requireContext(),
+                requireComponents.core.store,
+                requireComponents.useCases.searchUseCases.defaultSearch,
+                fetchClient = requireComponents.core.client,
+                mode = SearchSuggestionProvider.Mode.MULTIPLE_SUGGESTIONS,
+                engine = requireComponents.core.engine,
+                limit = 5,
+                filterExactMatch = true
+            )
+            .addSessionProvider(
+                resources,
+                requireComponents.core.store,
+                requireComponents.useCases.tabsUseCases.selectTab
+            )
+            .addHistoryProvider(
+                requireComponents.core.historyStorage,
+                requireComponents.useCases.sessionUseCases.loadUrl
+            )
+            .addClipboardProvider(
+                requireContext(),
+                requireComponents.useCases.sessionUseCases.loadUrl
+            )
+
+        awesomeBar.addProviders(
+            SyncedTabsStorageSuggestionProvider(
+                requireComponents.backgroundServices.syncedTabsStorage,
+                requireComponents.useCases.tabsUseCases.addTab,
+                requireComponents.core.icons
+            )
+        )
+    }
+
+    private fun initializeToolbar() {
+        TabsToolbarFeature(
+            toolbar = toolbar,
+            sessionId = sessionId,
+            store = requireComponents.core.store,
+            showTabs = ::showTabs,
+            lifecycleOwner = viewLifecycleOwner
+        )
+    }
+
+    private fun initializeFeatures() {
+        thumbnailsFeature.set(
+            feature = BrowserThumbnails(
+                requireContext(),
+                engineView,
+                requireComponents.core.store
+            ),
+            owner = viewLifecycleOwner,
+            view = requireView()
+        )
+
+        readerViewFeature.set(
+            feature = ReaderViewIntegration(
+                requireContext(),
+                requireComponents.core.engine,
+                requireComponents.core.store,
+                toolbar,
+                readerViewBar,
+                readerViewAppearanceButton
+            ),
+            owner = viewLifecycleOwner,
+            view = requireView()
+        )
+
+        webExtToolbarFeature.set(
+            feature = WebExtensionToolbarFeature(
+                toolbar,
+                requireContext().components.core.store
+            ),
+            owner = viewLifecycleOwner,
+            view = requireView()
+        )
+
+        windowFeature.set(
+            feature = WindowFeature(
+                store = requireComponents.core.store,
+                tabsUseCases = requireComponents.useCases.tabsUseCases
+            ),
+            owner = viewLifecycleOwner,
+            view = requireView()
+        )
+    }
+
+    private fun initializePageLoadMonitoring() {
+        requireComponents.core.store.flowScoped(viewLifecycleOwner) { flow ->
+            flow.map { it.selectedTab }
+                .distinctUntilChanged()
+                .collect { tab ->
+                    when (tab?.content?.progress) {
+                        0 -> PerformanceLogger.startMeasuring(PerformanceLogger.Tags.PAGE_LOAD_SPEED)
+                        100 -> PerformanceLogger.stopMeasuring(PerformanceLogger.Tags.PAGE_LOAD_SPEED)
+                    }
+                }
+        }
+    }
+
+    private suspend fun initializeComponents() = withContext(Dispatchers.Main) {
+        initializeAwesomeBar()
+
+        initializeToolbar()
+
+        initializeFeatures()
+    }
 
     override val shouldUseComposeUI: Boolean
         get() = PreferenceManager.getDefaultSharedPreferences(requireContext()).getBoolean(
@@ -63,110 +174,18 @@ class BrowserFragment : BaseBrowserFragment(), UserInteractionHandler {
         PerformanceLogger.startMeasuring(PerformanceLogger.Tags.BROWSER_FRAGMENT_DRAW)
 
         // Zorbey Torunoğlu:
-        // UI becomes visible/interactable quite a while later (2412.33ms on average when cold start).
-        // Listener below is to get the closest data to see how long does it take to become visible.
+        // listener to get the closest data to see how long does it take to become visible.
         view.viewTreeObserver.addOnGlobalLayoutListener {
             PerformanceLogger.stopMeasuring(PerformanceLogger.Tags.BROWSER_FRAGMENT_DRAW)
         }
 
-        // Zorbey Torunoğlu:
-        // Measuring the page load speed.
-        requireComponents.core.store.flowScoped(viewLifecycleOwner) { flow ->
-            flow.map { state ->
-                state.selectedTab
-            }.collect { tab ->
-                // TODO: add logger in Application class
-                when (tab?.content?.progress) {
-                    0 -> {
-                        PerformanceLogger.startMeasuring(PerformanceLogger.Tags.PAGE_LOAD_SPEED)
-                    }
-                    100 -> {
-                        PerformanceLogger.stopMeasuring(PerformanceLogger.Tags.PAGE_LOAD_SPEED)
-                    }
-                }
-            }
+        viewLifecycleOwner.lifecycleScope.launch {
+            initializeComponents()
         }
 
-        AwesomeBarFeature(awesomeBar, toolbar, engineView)
-            .addSearchProvider(
-                requireContext(),
-                requireComponents.core.store,
-                requireComponents.useCases.searchUseCases.defaultSearch,
-                fetchClient = requireComponents.core.client,
-                mode = SearchSuggestionProvider.Mode.MULTIPLE_SUGGESTIONS,
-                engine = requireComponents.core.engine,
-                limit = 5,
-                filterExactMatch = true,
-            )
-            .addSessionProvider(
-                resources,
-                requireComponents.core.store,
-                requireComponents.useCases.tabsUseCases.selectTab,
-            )
-            .addHistoryProvider(
-                requireComponents.core.historyStorage,
-                requireComponents.useCases.sessionUseCases.loadUrl,
-            )
-            .addClipboardProvider(requireContext(), requireComponents.useCases.sessionUseCases.loadUrl)
-
-        // We cannot really add a `addSyncedTabsProvider` to `AwesomeBarFeature` coz that would create
-        // a dependency on feature-syncedtabs (which depends on Sync).
-        awesomeBar.addProviders(
-            SyncedTabsStorageSuggestionProvider(
-                requireComponents.backgroundServices.syncedTabsStorage,
-                requireComponents.useCases.tabsUseCases.addTab,
-                requireComponents.core.icons,
-            ),
-        )
-
-        TabsToolbarFeature(
-            toolbar = toolbar,
-            sessionId = sessionId,
-            store = requireComponents.core.store,
-            showTabs = ::showTabs,
-            lifecycleOwner = this,
-        )
-
-        thumbnailsFeature.set(
-            feature = BrowserThumbnails(
-                requireContext(),
-                engineView,
-                requireComponents.core.store,
-            ),
-            owner = this,
-            view = view,
-        )
-
-        readerViewFeature.set(
-            feature = ReaderViewIntegration(
-                requireContext(),
-                requireComponents.core.engine,
-                requireComponents.core.store,
-                toolbar,
-                readerViewBar,
-                readerViewAppearanceButton,
-            ),
-            owner = this,
-            view = view,
-        )
-
-        webExtToolbarFeature.set(
-            feature = WebExtensionToolbarFeature(
-                toolbar,
-                requireContext().components.core.store,
-            ),
-            owner = this,
-            view = view,
-        )
-
-        windowFeature.set(
-            feature = WindowFeature(
-                store = requireComponents.core.store,
-                tabsUseCases = requireComponents.useCases.tabsUseCases,
-            ),
-            owner = this,
-            view = view,
-        )
+        // Zorbey Torunoğlu:
+        // Measuring the page load speed.
+        initializePageLoadMonitoring()
 
         engineView.setDynamicToolbarMaxHeight(resources.getDimensionPixelSize(R.dimen.browser_toolbar_height))
         PerformanceLogger.stopMeasuring(PerformanceLogger.Tags.BROWSER_FRAGMENT_CREATION)
